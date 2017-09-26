a0f9d75d5b2e74f08a02c9cf6e6f203f71f16e1a
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-09-25 19:00:04 -0700

    Fully implemented replayprocess_find, will iterate through pages of replays on hotsapi and insert relevant hotsapi metadata into replays table, to be used to download and process the actual replay at a later date. Allows for the start/stop of the process at will, so that it will pick right back up where it left off. Fairly error tolerant, as long as the hotsapi api remains consistent.

--- cgi/includes/hotsapi.php
+++ cgi/includes/hotsapi.php
@@ -4,29 +4,29 @@ class Hotsapi {
     const REPLAYS_PER_PAGE = 100;
     const HTTP_OK = 200;
     const HTTP_RATELIMITED = 429;
+    const MAX_REPLAY_AGE = 90; //90 days
+    public static $validMatchTypes = array("QuickMatch", "HeroLeague", "TeamLeague", "UnrankedDraft");
 
     /*
      * Returns assoc array of the headers and json body of the paged replays response
      * ['headers'] = assoc array of the various headers such as ['headers']['x-ratelimit-remaining'] = 299
      * ['json'] = assoc array of the json body object of the response
      */
-    public function getPagedReplays($page) {
+    public static function getPagedReplays($page) {
         return Hotsapi::ResponseHeadersAndJson(Hotsapi::API . '/replays/paged?page=' . $page);
     }
 
     /*
-     * Returns assoc array of the headers and json body of the response at url
+     * Returns assoc array of http code and json body of the response at url
      * ['code'] = http code of response
-     * ['headers'] = assoc array of the various headers such as ['headers']['x-ratelimit-remaining'] = 299
      * ['json'] = assoc array of the json body object of the response
      *
      * If a failed response occured, only the code key will be set, so always check the code first
      */
-    public function ResponseHeadersAndJson($url) {
+    public static function ResponseHeadersAndJson($url) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json')); //Tells it to output content as json
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //curl_setopt($ch, CURLINFO_HEADER_OUT, true); //Tells it to include the header output
         curl_setopt($ch, CURLOPT_URL, $url); //Curl url
         $res = curl_exec($ch);
         $res_info = curl_getinfo($ch);
@@ -37,21 +37,53 @@ class Hotsapi {
         $ret['code'] = $code;
 
         if ($code == Hotsapi::HTTP_OK) {
-            /*//Splits the headers and the json body, and then stores them neatly in an assoc array
-            list($headers, $body) = explode("\n\n", $res, 2);
-            $headers = explode("\n", $headers);
-            foreach ($headers as $header) {
-                list($key, $value) = explode(':', $header, 2);
-                $headers[trim($key)] = trim($value);
-            }
-
-            $ret['headers'] = $headers;*/
             $ret['json'] = json_decode($res, true);
 
             return $ret;
         }
 
         return $ret;
+    }
+
+    /*
+     * Takes and array $replays and returns a subarray that might be empty, and contains all replay objects
+     * that have a page index equal to or greater than $id. Optionally also filter for valid match types.
+     * Adds an extra key 'page_index' that is set to the replays original page index before the filtering occured
+     */
+    public static function getReplaysGreaterThanEqualToId($replays, $id, $filterValidMatchTypes = true, $filterByDays = Hotsapi::MAX_REPLAY_AGE) {
+        $arr = [];
+        $i = 1;
+        foreach ($replays as $replay) {
+            $modreplay = $replay;
+            $modreplay['page_index'] = $i;
+            $replaytype = $modreplay['game_type'];
+            $replaydate = $modreplay['game_date'];
+
+
+            if ($i >= $id && self::getReplayAgeInDays($replaydate) <= $filterByDays) {
+                if ($filterValidMatchTypes) {
+                    if (in_array($replaytype, Hotsapi::$validMatchTypes, true)) {
+                        $arr[] = $modreplay;
+                    }
+                }
+                else {
+                    $arr[] = $modreplay;
+                }
+            }
+            $i++;
+        }
+        return $arr;
+    }
+
+    /*
+     * Takes the replay date string and returns the amount of days since it was created
+     */
+    public static function getReplayAgeInDays($str) {
+        date_default_timezone_set("UTC");
+        $replaydate = new DateTime($str);
+        $nowdate = new DateTime("now");
+        $interval = $replaydate->diff($nowdate);
+        return $interval->days;
     }
 }
 ?>
--- cgi/replayprocess_find.php
+++ cgi/replayprocess_find.php
@@ -13,51 +13,124 @@ $db = new Database();
 $db->connectDefaultReplayProcess();
 
 //Prepare statements
-$db->prepare("SelectNewestReplay", "SELECT * FROM replays ORDER BY id DESC LIMIT 1");
+$db->prepare("SelectNewestReplay", "SELECT * FROM replays ORDER BY hotsapi_page DESC, hotsapi_idinpage DESC LIMIT 1");
+$db->prepare("InsertNewReplay", "INSERT INTO replays (hotsapi_id, hotsapi_page, hotsapi_idinpage, fingerprint, hotsapi_url, status) VALUES (?, ?, ?, ?, ?, ?)");
+$db->bind("InsertNewReplay", "iiisss", $r_id, $r_page, $r_idinpage, $r_fingerprint, $r_s3url, $r_status);
 
 //Constants and qol
-$toomanyrequestssleepdur = 60;
-$sleepdur = 5;
-$minisleepdur = 1;
+const OUT_OF_REPLAYS_SLEEP_DURATION = 3600; //seconds
+const UNKNOWN_ERROR_CODE = 300; //seconds
+const TOO_MANY_REQUEST_SLEEP_DURATION = 60; //seconds
+const SLEEP_DURATION = 5; //seconds
+const MINI_SLEEP_DURATION = 1; //seconds
 $e = PHP_EOL;
+$dosleep = false;
 
+//Helper functions
+function smartSleep($duration, $mainsleep = false, $mainsleepDuration = SLEEP_DURATION) {
+    global $dosleep;
+
+    if ($mainsleep) {
+        if ($dosleep) {
+            sleep($duration);
+
+            $dosleep = false;
+        }
+    }
+    else {
+        sleep($duration - $mainsleepDuration);
+
+        $dosleep = true;
+    }
+}
+
+function addToPageIndex($amount) {
+    global $pageindex;
+    setPageIndex($pageindex + $amount);
+}
+
+function setPageIndex($amount) {
+    global $pagenum, $pageindex;
+
+    $pageindex = $amount;
+    if ($pageindex > Hotsapi::REPLAYS_PER_PAGE) {
+        $pagenum++;
+        $pageindex = 1;
+    }
+}
+
+//Begin main script
 echo 'Replay process <<FIND>> has started'.$e
     .'--------------------------------------'.$e;
 
+//Get newest replay if there is one to determine where to start looking in hotsapi
+$result = $db->execute("SelectNewestReplay");
+$resrows = $db->countResultRows($result);
+$pagenum = 1; //Default start at page 1
+$pageindex = 1; //Default replay page id
+
+if ($resrows > 0) {
+    $row = $db->fetchArray($result);
+
+    $pagenum = $row['hotsapi_page'];
+    setPageIndex($row['hotsapi_idinpage'] + 1);
+}
+
+$db->freeResult($result);
+
 //Look for replays to download and handle
 while (true) {
-    //Get newest replay if there is one to determine where to start looking in hotsapi
-    $result = $db->execute("SelectNewestReplay");
-    $resrows = $db->countResultRows($result);
-    $pagenum = 1; //Default start at page 1
-    $replayid = 1; //Default replay id
-
-    if ($resrows > 0) {
-        $row = $db->fetchArray($result);
-
-        $replayid = $row['hotsapi_id'] + 1;
-        $pagenum = floor($replayid / Hotsapi::REPLAYS_PER_PAGE) + 1;
-    }
-
-    $db->freeResult($result);
-
-    echo 'Requesting page '.$pagenum.' from hotsapi, starting at replay '.$replayid.'...'.$e;
+    echo 'Requesting page '.$pagenum.' from hotsapi, starting at page index '.$pageindex.'...'.$e;
 
     $api = Hotsapi::getPagedReplays($pagenum);
 
+    $prevpage = $pagenum;
+
     if ($api['code'] == Hotsapi::HTTP_OK) {
         //Process json data and put it in the database
+        $replays = $api['json']['replays'];
+        $replaylen = count($replays);
+        if ($replaylen > 0) {
+            $relevant_replays = Hotsapi::getReplaysGreaterThanEqualToId($replays, $pageindex);
+            if (count($relevant_replays) > 0) {
+                foreach ($relevant_replays as $replay) {
+                    $r_id = $replay['id'];
+                    $r_page = $pagenum;
+                    $r_idinpage = $replay['page_index'];
+                    $r_fingerprint = $replay['fingerprint'];
+                    $r_s3url = $replay['url'];
+                    $r_status = "queued";
+
+                    $db->execute("InsertNewReplay");
+                }
+                addToPageIndex($replaylen); //Finished with page, rollover page index
+                echo 'Page #' . $prevpage . ' processed (' . count($relevant_replays) . ' relevant replays).'.$e;
+            }
+            else {
+                //No relevant replays found here, set next replayid to be greater than the highest id in the replayset
+                addToPageIndex($replaylen); //Finished with page, rollover page index
+                echo 'Page #' . $prevpage . ' had no more relevant replays.'.$e;
+            }
+        }
+        else {
+            //No more replay pages to process! Long sleep
+            echo 'Out of replays to process! Waiting for new hotsapi replay at page index #' . $pageindex . '...'.$e;
+            smartSleep(OUT_OF_REPLAYS_SLEEP_DURATION);
+        }
     }
     else if ($api['code'] == Hotsapi::HTTP_RATELIMITED) {
         //Error too many requests, wait awhile before trying again
-        echo 'Error: HTTP Code ' . $api['code'] . '. Rate limited.';
-        sleep($toomanyrequestssleepdur - $sleepdur);
+        echo 'Error: HTTP Code ' . $api['code'] . '. Rate limited.'.$e;
+        smartSleep(TOO_MANY_REQUEST_SLEEP_DURATION);
     }
     else {
-        echo 'Error: HTTP Code ' . $api['code'];
+        echo 'Error: HTTP Code ' . $api['code'].'.'.$e;
+        smartSleep(UNKNOWN_ERROR_CODE);
     }
 
-    sleep($sleepdur);
+    if ($dosleep) {
+        smartSleep(SLEEP_DURATION, true);
+    }
 }
 
 ?>
