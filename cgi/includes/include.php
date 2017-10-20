5c25e37b4c2a9e9bcfd6b43dc3806d77a7b75089
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-10-20 13:44:43 -0700

    Removed cgi folder from main repo to branch it off to its own repo

--- cgi/includes/include.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../lib/Fizzik/fizzik-autoloader.php';
-require_once __DIR__ . '/Credentials.php';
-require_once __DIR__ . '/HotstatusPipeline.php';
--- cgi/lib/README.md
+++
@@ -1,3 +0,0 @@
-<b>CGI Libraries (cgi/lib)</b>
-- [AWS Php SDK](https://aws.amazon.com/sdk-for-php/)
-- [Fizzik](https://github.com/maximtiourin/Fizzik)
